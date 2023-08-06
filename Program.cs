392053e4e4e3580c45d53f8653abb553ee0ef60b
jiujiuaia <jiujiuaia@gmail.com>
2023-08-06 12:05:07 +0800

    first

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendMetricToLA
+{
+    public class Program
+    {
+        public static async Task Main()
+        {
+            SendMetricToLA.SendMetric(10);
+        }
+    }
+}
